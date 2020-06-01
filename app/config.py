02414ad8005b6c96734e5a98e8905898d520dde9
NN-Guillaume <35535667+NN-Guillaume@users.noreply.github.com>
2020-06-01 14:08:13 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,3 @@
 # config file
 
-import os
-
-if os.environ.get('GOOGLE_KEY') is None:
-    key_value = 'AIzaSyDf-8PO-M4h0cgIXw1dGZ4NCt1xVUWvFbY'
-else:
-    key_value = os.environ['GOOGLE_KEY']
+key_value = 'AIzaSyDf-8PO-M4h0cgIXw1dGZ4NCt1xVUWvFbY'
